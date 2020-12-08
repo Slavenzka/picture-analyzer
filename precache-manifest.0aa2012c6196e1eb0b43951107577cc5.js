75b6500d54016cdd45233c913db9a5b2c42be88d
Ivan <i.romanov@protonmail.com>
2020-12-08 22:20:13 +0300

    Updates

---
+++ precache-manifest.0aa2012c6196e1eb0b43951107577cc5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87d2562422c50c49089ceb9779617d5",
+    "url": "/picture-analyzer/index.html"
+  },
+  {
+    "revision": "b776e8258a026064c2c4",
+    "url": "/picture-analyzer/static/css/2.2cba102f.chunk.css"
+  },
+  {
+    "revision": "723f0eca89a9fc3f3b6e",
+    "url": "/picture-analyzer/static/css/main.c368edbd.chunk.css"
+  },
+  {
+    "revision": "b776e8258a026064c2c4",
+    "url": "/picture-analyzer/static/js/2.aed9b21a.chunk.js"
+  },
+  {
+    "revision": "618cb864cf8e37a20f4661316a5ab0e5",
+    "url": "/picture-analyzer/static/js/2.aed9b21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "723f0eca89a9fc3f3b6e",
+    "url": "/picture-analyzer/static/js/main.c287fe71.chunk.js"
+  },
+  {
+    "revision": "2430c9aee0e7eb276b3c",
+    "url": "/picture-analyzer/static/js/runtime-main.a9080f8b.js"
+  },
+  {
+    "revision": "6144c976c393d4687b2638e5d8cc3457",
+    "url": "/picture-analyzer/static/media/FuturaPT-Bold.6144c976.woff"
+  },
+  {
+    "revision": "d56aa7dac12ba646cc34628f7594098b",
+    "url": "/picture-analyzer/static/media/FuturaPT-Bold.d56aa7da.woff2"
+  },
+  {
+    "revision": "231495e51444c162bed5df57ee968c8e",
+    "url": "/picture-analyzer/static/media/FuturaPT-Book.231495e5.woff2"
+  },
+  {
+    "revision": "54a123e5410e5dadf16d8b368fa0ab54",
+    "url": "/picture-analyzer/static/media/FuturaPT-Book.54a123e5.woff"
+  },
+  {
+    "revision": "1a9a265e0f3e302e697ae7c627e9608f",
+    "url": "/picture-analyzer/static/media/FuturaPT-Demi.1a9a265e.woff2"
+  },
+  {
+    "revision": "74a7ad78ff615532bb831f254108ca0a",
+    "url": "/picture-analyzer/static/media/FuturaPT-Demi.74a7ad78.woff"
+  },
+  {
+    "revision": "35e7a9d1f64094beeada315f43408294",
+    "url": "/picture-analyzer/static/media/FuturaPTLight.35e7a9d1.woff"
+  },
+  {
+    "revision": "488414dacc28d8fd30a89b2755dce961",
+    "url": "/picture-analyzer/static/media/FuturaPTLight.488414da.woff2"
+  },
+  {
+    "revision": "09d6db7d3a908ea9df2f0365fcc3de74",
+    "url": "/picture-analyzer/static/media/icon-arrow-calendar.09d6db7d.svg"
+  }
+]);
